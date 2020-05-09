ae1d70b79c17c214590436f29a76e0aca5674836
Juan Grosso <grosso379@gmail.com>
2020-05-09 18:39:18 -0400

    Added keyboard input and function that checks if the input is valid or not

--- js/main.js
+++ js/main.js
@@ -1,48 +1,86 @@
+//Check if keyboard input is valid or not
+function isValidChar(tecla) {
+  let ch = tecla.key;
+  let pattern = /[0-9]|\%|\/|\*|\+|\-/;
+  if (ch == "Enter") {
+    solveExpression();
+  } else if (pattern.test(ch)) {
+    printHistory(getResult() + getHistory());
+    printResult("");
+  } else {
+    tecla.preventDefault();
+  }
+}
+
+// Keep focus on input
+const entrada = document.getElementById("entry");
+entrada.onblur = function () {
+  entrada.focus();
+};
+
+//Function for getting rhe result
+const solveExpression = function () {
+  let history = getOriginalNumber(getHistory());
+  history = getEvaluableHistory(history);
+  try {
+    printResult(math.evaluate(history));
+  } catch (err) {
+    alert("The expresion inserted can't be evaluated");
+  }
+  printHistory("");
+};
+
 // Format result to separate with comas each 3 digits
-var getFormattedNumber = function (num) {
+const getFormattedNumber = function (num) {
   if (num == "") {
     return num;
   } else {
-    var n = Number(num);
-    var value = n.toLocaleString("en");
+    let n = Number(num);
+    let value = n.toLocaleString("en");
     return value;
   }
 };
 
 // Transform formatted number into regular number to calculate te result
-var getOriginalNumber = function (num) {
-  var value = num.replace(/,/g, "");
+const getOriginalNumber = function (num) {
+  let value = num.replace(/,/g, "");
   return value;
 };
 
 // Changing multiplication, division and percentage symbols to make them work correctly
-var getEvaluableHistory = function (string) {
-  var value = string.replace(/×/g, "*");
+const getEvaluableHistory = function (string) {
+  let value = string.replace(/×/g, "*");
   value = value.replace(/÷/g, "/");
   value = value.replace(/%/g, "*0.01*");
   return value;
 };
 
-var getHistory = function () {
-  return document.getElementById("entry").innerText;
+const getHistory = function () {
+  return document.getElementById("entry").value;
 };
 
-var printHistory = function (char) {
-  document.getElementById("entry").innerText = char;
+const printHistory = function (char) {
+  document.getElementById("entry").value = char;
 };
 
-var getResult = function () {
-  return document.getElementById("result").innerText;
+const getResult = function () {
+  // if result has NaN on it return empty string, otherwise return what's in result
+  let text = document.getElementById("result").innerText;
+  if (text == NaN) {
+    return "";
+  } else {
+    return text;
+  }
 };
 
-var printResult = function (num) {
+const printResult = function (num) {
   num = getFormattedNumber(num);
   document.getElementById("result").innerText = num;
 };
 
-// Code for the functioning of the calculator
+// Operators functioning
 
-var operators = document.getElementsByClassName("operator");
+let operators = document.getElementsByClassName("operator");
 for (let i = 0; i < operators.length; i++) {
   operators[i].addEventListener("click", function () {
     // Define behavior for operators pressed (AC:clear history ; C:Delete las char ; =:Evaluate history; Other operators add them to the history)
@@ -52,14 +90,7 @@ for (let i = 0; i < operators.length; i++) {
     } else if (this.id == "backspace") {
       printHistory(getHistory().slice(0, -1));
     } else if (this.id == "equal") {
-      var history = getOriginalNumber(getHistory());
-      history = getEvaluableHistory(history);
-      try {
-        printResult(math.evaluate(history));
-      } catch (err) {
-        alert("The expresion inserted can't be evaluated");
-      }
-      printHistory("");
+      solveExpression();
     } else {
       printHistory(getResult() + getHistory() + this.innerText);
       printResult("");
@@ -68,10 +99,12 @@ for (let i = 0; i < operators.length; i++) {
 }
 
 // Add numbers pressed to the history
-var numbers = document.getElementsByClassName("number");
+let numbers = document.getElementsByClassName("number");
 for (let j = 0; j < numbers.length; j++) {
   numbers[j].addEventListener("click", function () {
     printResult("");
     printHistory(getHistory() + this.innerText);
   });
 }
+
+//Resolve when enter is pressed
